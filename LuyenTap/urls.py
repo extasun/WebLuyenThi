2a43a65ff738ab51b27ff0a8acb0cc70aef20a04
extasun <tiennhat12a@gmail.com>
2024-08-25 16:01:57 +0700

    25/08

--- LuyenTap/urls.py
+++ LuyenTap/urls.py
@@ -3,7 +3,7 @@ from .views import Thi, Online, ChamThi
 app_name = 'luyentap'
 urlpatterns = [
     path('thi/', Thi.as_view(), name= 'thi'),
-    path('/online/', Online.as_view(), name= 'dgtd'),
-    path('/online/<int:idde>/', Online.as_view(), name= 'dgtd_id'),
+    path('online', Online.as_view(), name= 'dgtd'),
+    path('online/<int:idde>/', Online.as_view(), name= 'dgtd_id'),
     path('chamthi/', ChamThi.as_view(), name='chamthi'),
 ]
